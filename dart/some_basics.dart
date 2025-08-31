4fce482308b2587133a8dc9a34357e392cfed59c
Siamoo <walidsyam.f@gmail.com>
2025-09-01 02:34:55 +0300

    forEach

--- dart/some_basics.dart
+++ dart/some_basics.dart
@@ -3,7 +3,11 @@ Map map = {'a': 1, 'b': 2, 'c': 3};
 Set set = {1, 2, 3};
 
 void main() {
-  for (var i in list) {
+  // for (var i in list) {
+  //   print(i);
+  // }
+  
+  list.forEach((i) {
     print(i);
-  }
+  });
 }
