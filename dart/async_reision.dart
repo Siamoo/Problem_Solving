77965b8efde5ecf89893372b99aa2e00ce51cf25
Siamoo <walidsyam.f@gmail.com>
2025-09-02 23:24:01 +0300

    Async & Await

--- dart/async_reision.dart
+++ dart/async_reision.dart
@@ -1,12 +1,26 @@
-main() {
-  Future.delayed(
+main() async {
+  await Future.delayed(
     Duration(seconds: 1),
     () => 2,
   ).then((value) => print(value)).catchError((e) => print(e));
 
   print('value = 1');
 
+  try {
+    final f1 = await Future.delayed(Duration(seconds: 1), () => 2);
+    print(f1);
+  } catch (e) {
+    print(e);
+  }
+
   final s1 = Stream.periodic(Duration(milliseconds: 500), (x) => x);
-  final sub = s1.listen((_) => print('value'));
-  sub.onData((data) => data> 10 ? sub.cancel() : print(data));
+  //final sub = s1.listen((_) => print('value'));
+  //sub.onData((data) => data > 10 ? sub.cancel() : print(data));
+
+  await for (var value in s1) {
+    print(value);
+    if (value > 10) {
+      break;
+    }
+  }
 }
