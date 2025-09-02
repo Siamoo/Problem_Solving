73f1a58aeaa87588e98783974d5f79caff6e220e
Siamoo <walidsyam.f@gmail.com>
2025-09-02 12:29:39 +0300

    Enum

--- dart/oop_revision.dart
+++ dart/oop_revision.dart
@@ -1,53 +1,16 @@
-mixin A {
-  info();
-}
-
-mixin B {
-  info2();
-}
-
-abstract class C {
-  info3();
-}
-
-/// implements for multiple inheritance with Abstract and Mixins
-class D extends C with A, B {
-  @override
-  info() {
-    print('info');
-  }
-
-  @override
-  info2() {
-    print('info2');
-  }
-
-  @override
-  info3() {
-    print('info3');
-  }
-}
-
-/// extends for multiple inheritance with Mixins only
-class E with A,B {
-  @override
-  info() {
-    print('info');
-  }
-
-  @override
-  info2() {
-    print('info2');
-  }
-}
-
 main() {
-  D d = D();
-  d.info();
-  d.info2();
-  d.info3();
+  print(info(Color.red));
+}
 
-  E e = E();
-  e.info();
-  e.info2();
+enum Color { red, green, blue }
+
+info(Color color) {
+  switch (color) {
+    case Color.red:
+      return 'i\'m red';
+    case Color.green:
+      return 'i\'m green';
+    case Color.blue:
+      return 'i\'m blue';
+  }
 }
