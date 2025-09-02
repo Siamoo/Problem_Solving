d39cf75fbe008280eb158910195a1d81205eaa6e
Siamoo <walidsyam.f@gmail.com>
2025-09-02 12:13:47 +0300

    Mixins

--- dart/oop_revision.dart
+++ dart/oop_revision.dart
@@ -1,25 +1,53 @@
-abstract class A {
+mixin A {
   info();
 }
 
-abstract class B {
+mixin B {
   info2();
 }
-/// implements for multiple inheritance
-class C implements A, B { 
+
+abstract class C {
+  info3();
+}
+
+/// implements for multiple inheritance with Abstract and Mixins
+class D extends C with A, B {
   @override
   info() {
-    print('form class C info');
+    print('info');
   }
 
   @override
   info2() {
-    print('form class C info2');
+    print('info2');
+  }
+
+  @override
+  info3() {
+    print('info3');
+  }
+}
+
+/// extends for multiple inheritance with Mixins only
+class E with A,B {
+  @override
+  info() {
+    print('info');
+  }
+
+  @override
+  info2() {
+    print('info2');
   }
 }
 
 main() {
-  C c = C();
-  c.info();
-  c.info2();
+  D d = D();
+  d.info();
+  d.info2();
+  d.info3();
+
+  E e = E();
+  e.info();
+  e.info2();
 }
