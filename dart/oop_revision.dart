d16172ec02a6e7228f27c56fd6d107201ef16220
Siamoo <walidsyam.f@gmail.com>
2025-09-02 12:44:34 +0300

    Generic

--- dart/oop_revision.dart
+++ dart/oop_revision.dart
@@ -1,16 +1,25 @@
 main() {
-  print(info(Color.red));
+  var a = A<int, String, bool>();
+  a.info(1, 'a', true);
+  print(a.t);
+  print(a.e);
+  print(a.f);
+
+  var b = A();
+  b.info(true, 1, 'b');
+  print(b.t);
+  print(b.e);
+  print(b.f);
 }
 
-enum Color { red, green, blue }
-
-info(Color color) {
-  switch (color) {
-    case Color.red:
-      return 'i\'m red';
-    case Color.green:
-      return 'i\'m green';
-    case Color.blue:
-      return 'i\'m blue';
+class A<T, E, F> {
+  late T t;
+  late E e;
+  late F f;
+  
+  info(T t, E e, F f) {
+    this.t = t;
+    this.e = e;
+    this.f = f;
   }
 }
