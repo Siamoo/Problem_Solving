c59f29863828ff9c6d9e7319a8c490b4de7f691e
Siamoo <walidsyam.f@gmail.com>
2025-09-02 11:26:28 +0300

    Super & this & static

--- dart/oop_revision.dart
+++ dart/oop_revision.dart
@@ -1,36 +1,52 @@
 /// Super class for B & Indirect Super class for C
 class A {
-  info() {
-    print('siam');
+  info(num) {
+    print('siam_$num');
   }
 }
 
 /// Direct Super class for C
 class B extends A {
-  info2() {
-    print('siam2');
+  info2(num) {
+    print('siam_$num');
   }
 }
 
 /// Sub class
 class C extends B {
-  @override
-  info() { 
-    print('walid');
+  late int num3, num4;
+  static late int num7, num8;
+  fun() {
+    super.info(this.num3); // using this because num3 is instance variable not static
+    super.info2(this.num4);
   }
 
-  @override
-  info2() {
-    print('walid2');
+  fun2(num5, num6) {
+    super.info(num5);
+    super.info2(num6);
+  }
+
+  fun3() {
+    super.info(num7); // not using this because num7 is static
+    super.info2(num8);
   }
 }
 
 main() {
+
   B b = B();
-  b.info();
-  b.info2();
+  b.info(1);
+  b.info2(2);
 
   C c = C();
-  c.info();
-  c.info2();
+
+  c.num3 = 3;
+  c.num4 = 4;
+  c.fun();
+
+  c.fun2(5, 6);
+
+  C.num7 = 7;
+  C.num8 = 8;
+  c.fun3();
 }
