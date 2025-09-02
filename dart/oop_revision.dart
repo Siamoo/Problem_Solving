f5a8bf602792fb3bce2139b3d09209faacec21f7
Siamoo <walidsyam.f@gmail.com>
2025-09-02 11:49:21 +0300

    Super Constructor

--- dart/oop_revision.dart
+++ dart/oop_revision.dart
@@ -1,52 +1,27 @@
-/// Super class for B & Indirect Super class for C
 class A {
-  info(num) {
-    print('siam_$num');
+  A() {
+    print('this is A class');
   }
 }
 
-/// Direct Super class for C
 class B extends A {
-  info2(num) {
-    print('siam_$num');
+  B(String name) {
+    print('from B this is $name');
   }
 }
 
-/// Sub class
 class C extends B {
-  late int num3, num4;
-  static late int num7, num8;
-  fun() {
-    super.info(this.num3); // using this because num3 is instance variable not static
-    super.info2(this.num4);
+  C(String name) : super(name) {
+    // no thing in C
   }
+}
 
-  fun2(num5, num6) {
-    super.info(num5);
-    super.info2(num6);
-  }
-
-  fun3() {
-    super.info(num7); // not using this because num7 is static
-    super.info2(num8);
+class D extends C {
+  D(String name) : super(name) {
+    print('from D this is $name');
   }
 }
 
 main() {
-
-  B b = B();
-  b.info(1);
-  b.info2(2);
-
-  C c = C();
-
-  c.num3 = 3;
-  c.num4 = 4;
-  c.fun();
-
-  c.fun2(5, 6);
-
-  C.num7 = 7;
-  C.num8 = 8;
-  c.fun3();
+  D d = D('D');
 }
